f12cef3722cad25ddaf8c0521ab0887214ef98f4
huyenvtn <huyenvtn0210@gmail.com>
2023-02-07 15:39:53 +0700

    ex 9.3

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,24 +1,50 @@
+interface BMIArg {
+  height: number,
+  weight: number
+}
+
+const checkBMIArguments = (args: Array<string>): BMIArg => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else throw new Error('Provided values were not numbers!')
+}
+
 const calculateBmi = (height: number, weight: number) => {
   if (height <= 0) {
-    return 'Wrong height'
+    console.log('Wrong height')
   }
   let bmi = weight/Math.pow((height/100), 2);
   if (bmi < 16.0) {
-    return 'Underweight (Severe thinness)'
+    console.log('Underweight (Severe thinness)')
   } else if (bmi >= 16.0 && bmi <= 16.9) {
-    return 'Underweight (Moderate thinness)'
+    console.log('Underweight (Moderate thinness)')
   } else if (bmi >= 17.0 && bmi <= 18.4) {
-    return 'Underweight (Mild thinness)'
+    console.log('Underweight (Mild thinness)')
   } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return 'Normal range'
+    console.log('Normal range')
   } else if (bmi >= 25.0 && bmi <= 29.9) {
-    return 'Overweight (Pre-obese)'
+    console.log('Overweight (Pre-obese)')
   } else if (bmi >= 30.0 && bmi <= 34.9) {
-    return 'Obese (Class I)'
+    console.log('Obese (Class I)')
   } else if (bmi >= 35.0 && bmi <= 39.9) {
-    return 'Obese (Class II)'
+    console.log('Obese (Class II)')
   } else if (bmi >= 40.0) {
-    return 'Obese (Class III)'
+    console.log('Obese (Class III)')
   }
 }
-console.log(calculateBmi(180,174))
+
+try {
+  const { height, weight } = checkBMIArguments(process.argv)
+  calculateBmi(height, weight)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage)
+}
+// calculateBmi(180,174)
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,3 +1,24 @@
+interface ExerciseArg {
+  hours: Array<number>,
+  target: number
+}
+
+const checkArguments = (args: Array<string>): ExerciseArg => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  let arr = args.slice(2); 
+  for (let i=0; i<arr.length; i++) {
+    if(isNaN(Number(arr[i]))){
+      throw new Error('Provided values were not numbers!')
+    }
+  }
+  const arrOfNum = arr.map(str => {
+    return Number(str);
+  });
+  return {
+    hours: arrOfNum.slice(1),
+    target: arrOfNum[0]
+  }
+}
 const calculateExercises = (hours: Array<number>, target: number) => {
   const hoursTrain = hours.filter(hour => hour > 0);
   let totalHours = 0;
@@ -15,5 +36,16 @@ const calculateExercises = (hours: Array<number>, target: number) => {
     average: hours.length > 0 ? totalHours/hours.length : 0
   }
 }
-const time = [3, 0, 2, 4.5, 0, 3, 1];
-console.log(calculateExercises(time, 1))
+try {
+  const { hours, target } = checkArguments(process.argv)
+  const rs = calculateExercises(hours, target)
+  console.log(rs)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage)
+}
+// const time = [3, 0, 2, 4.5, 0, 3, 1];
+// calculateExercises(time, 1)
